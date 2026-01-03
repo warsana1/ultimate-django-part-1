0b18466a8accbb66f57458cb1da64902087604eb
warsana <inyomanwarsana1@gmail.com>
2026-01-03 17:53:54 +0800

    Rename phone_number and last_updated

--- store/models.py
+++ store/models.py
@@ -21,7 +21,7 @@ class Product(models.Model):
     description = models.TextField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
-    last_updated = models.DateTimeField(auto_now=True)
+    last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(
         Collection,
         on_delete=models.PROTECT,
@@ -42,7 +42,7 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1,
