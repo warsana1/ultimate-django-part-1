c4d115e1bb0cfa071b695ec0c40040c6ab8f1c43
warsana <inyomanwarsana1@gmail.com>
2026-01-03 22:13:58 +0800

    Managers and query_set

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
 # Create your views here.
 
 
-def calculate():
-    x = 1
-    y = 2
-    return x
-
-
 def say_hello(request):
-    x = calculate()
+    query_set = Product.objects.count()
+
     return render(request, 'hello.html', {"name": "I Nyoman Warsana"})
