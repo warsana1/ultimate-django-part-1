3cfc9970ad4bdf5b92c2b91019f9c1df2df2ec0b
warsana <inyomanwarsana1@gmail.com>
2026-01-04 09:24:42 +0800

    Referencing fields using f object

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Q
+from django.db.models import Q, F
 from store.models import Product
 from store.models import Customer
 from store.models import Collection
@@ -12,8 +12,7 @@ from store.models import OrderItem
 
 def say_hello(request):
 
-    queryset = Product.objects.filter(
-        Q(inventory__lt=10) | Q(unit_price__lt=20))
+    queryset = Product.objects.filter(inventory=F('unit_price'))
 
     customer_queryset = Customer.objects.filter(email__icontains='.com')
 
