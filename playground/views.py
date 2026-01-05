423ba654592fa95551363ff73674e335a45e3d17
warsana <inyomanwarsana1@gmail.com>
2026-01-05 10:13:35 +0800

    Custom manager

--- playground/views.py
+++ playground/views.py
@@ -5,14 +5,9 @@ from store.models import Product
 from tags.models import TaggedItem
 
 # Create your views here.
+queryset = TaggedItem.objects.get_tags_for(Product, 1)
 
 
 def say_hello(request):
-
-    content_type = ContentType.objects.get_for_model(Product)
-
-    queryset = TaggedItem.objects.select_related('tag').filter(
-        content_type=content_type,
-        object_id=1)
 
     return render(request, 'hello.html', {"name": "I Nyoman Warsana", "result": list(queryset)})
