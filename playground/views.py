5382eca0f8106e6ea125a3e1bbb85336d3b9d63e
warsana <inyomanwarsana1@gmail.com>
2026-01-05 16:08:58 +0800

    Transactions

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from django.db.models import F, ExpressionWrapper, DecimalField
 from django.contrib.contenttypes.models import ContentType
-from store.models import Product, Collection
+from django.db import transaction
+from store.models import Product, Collection, Order, OrderItem
 from tags.models import TaggedItem
 
 
 def say_hello(request):
-    collection = Collection(pk=11)
-    collection.delete()
 
-    Collection.objects.filter(id__gt=5).delete()
+    with transaction.atomic():
+        order = Order()
+        order.customer_id = 1
+        order.save()
+
+        item = OrderItem()
+        item.order = order
+        item.product_id = -1
+        item.quantity = 1
+        item.unit_price = 10
+        item.save()
 
     return render(request, 'hello.html', {"name": "I Nyoman Warsana", })
