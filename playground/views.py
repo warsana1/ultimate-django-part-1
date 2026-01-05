916612e3bcf33c5cfc20904664058f52d96d013e
warsana <inyomanwarsana1@gmail.com>
2026-01-05 13:30:17 +0800

    Creating objects

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.db.models import F, ExpressionWrapper, DecimalField
 from django.contrib.contenttypes.models import ContentType
-from store.models import Product
+from store.models import Product, Collection
 from tags.models import TaggedItem
-
-# Create your views here.
-queryset = TaggedItem.objects.get_tags_for(Product, 1)
 
 
 def say_hello(request):
+    collection = Collection()
+    collection.title = "Video Games"
+    collection.featured_product = Product(pk=1)
+    collection.save()
 
-    return render(request, 'hello.html', {"name": "I Nyoman Warsana", "result": list(queryset)})
+    return render(request, 'hello.html', {"name": "I Nyoman Warsana", })
