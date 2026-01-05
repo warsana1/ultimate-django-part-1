afaef5201ac2d7bac6dbc3f5b958d204d9b7997d
warsana <inyomanwarsana1@gmail.com>
2026-01-05 18:15:59 +0800

    Setting up the admin site

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
-from django.db import connection
 
 
 def say_hello(request):
-    with connection.cursor() as cursor:
-        cursor.callproc('get_customers', [1])
 
-        return render(request, 'hello.html', {"name": "I Nyoman Warsana", })
+    return render(request, 'hello.html', {"name": "I Nyoman Warsana", })
--- storefront/settings.py
+++ storefront/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sessions',
     'playground',
     'debug_toolbar',
     'store',
