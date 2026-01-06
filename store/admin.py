e425c6ce861d8bc4f9c1b44a28f813f0463985c0
warsana <inyomanwarsana1@gmail.com>
2026-01-06 08:43:50 +0800

    Exercise: Display orders by customer

--- store/admin.py
+++ store/admin.py
@@ -25,10 +25,21 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
+    list_display = ['first_name', 'last_name', 'membership', 'orders']
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders_count=Count('order')
+        )
+
+    @admin.display(ordering='orders_count')
+    def orders(self, customer):
+        url = (reverse('admin:store_order_changelist') + '?'
+               + urlencode({'customer__id': str(customer.id)}))
+        return format_html('<a href="{}">{}</a>', url, customer.orders_count)
 
 
 @admin.register(models.Order)
