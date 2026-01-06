aac188a5580c702080ef10cf3a24cf73346e72e3
warsana <inyomanwarsana1@gmail.com>
2026-01-06 08:28:01 +0800

    Providing links to other pages

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from . import models
 from django.db.models import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
+from . import models
 
 
 @admin.register(models.Product)
@@ -37,11 +39,14 @@ class OrderAdmin(admin.ModelAdmin):
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ['title', 'product_count']
+    list_display = ['title', 'products_count']
 
     @admin.display(ordering='products_count')
-    def product_count(self, collection):
-        return collection.products_count
+    def products_count(self, collection):
+        url = (reverse('admin:store_product_changelist') + '?'
+               + urlencode({'collection__id': str(collection.id)}))
+
+        return format_html('<a href="{}">{}</a>', url, collection.products_count)
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
