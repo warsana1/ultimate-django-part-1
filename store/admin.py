5e9eb5f44d9e6901ea27a91b717e199c2e47a750
warsana <inyomanwarsana1@gmail.com>
2026-01-06 08:48:07 +0800

    Adding search to the list page

--- store/admin.py
+++ store/admin.py
@@ -27,8 +27,9 @@ class ProductAdmin(admin.ModelAdmin):
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership', 'orders']
     list_editable = ['membership']
-    ordering = ['first_name', 'last_name']
     list_per_page = 10
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
